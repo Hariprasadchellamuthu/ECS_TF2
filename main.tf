96fbc7da98d8ce52e855b3cac725d7b648f84626
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-12-18 23:01:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -196,10 +196,10 @@ resource "aws_ecs_service" "python_ecs_service" {
   task_definition = aws_ecs_task_definition.python_task_definition.arn
   launch_type     = "EC2"
 
-  #network_configuration {
-    #subnets = [aws_subnet.subnet_a.id]
-    #security_groups = [aws_security_group.ecs_security_group.id]
-  #}
+  network_configuration {
+    subnets = [aws_subnet.subnet_a.id]
+    security_groups = [aws_security_group.ecs_security_group.id]
+  }
 
   deployment_controller {
     type = "ECS"
@@ -272,10 +272,10 @@ resource "aws_ecs_service" "jenkins_ecs_service" {
   task_definition = aws_ecs_task_definition.jenkins_task_definition.arn
   launch_type     = "EC2"
 
-  #network_configuration {
-    #subnets = [aws_subnet.subnet_b.id]
-    #security_groups = [aws_security_group.ecs_security_group.id]
-  #}
+  network_configuration {
+    subnets = [aws_subnet.subnet_b.id]
+    security_groups = [aws_security_group.ecs_security_group.id]
+  }
 
   deployment_controller {
     type = "ECS"
